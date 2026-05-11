65bbaab0f6e1109d7319eaacf9772c0f88034847
Bry_devv <Victus@DESKTOP-7E1DBLR>
2026-05-11 17:23:22 -0600

    Agregar archivos de proyecto.

---
+++ Producto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MangaNya
+{
+    internal class Producto
+    {
+        string nombre { get; set; } = string.Empty;
+        string codigo { get; set; } = string.Empty;
+        string marca { get; set; } = string.Empty;
+        string precioCompra { get; set; } = string.Empty;
+        string precioVenta { get; set; } = string.Empty;
+        string cantidad { get; set; } = string.Empty;
+    }
+}
