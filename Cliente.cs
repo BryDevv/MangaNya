134df4b7d9a86069c149924dc522b6ea6e2017d1
Bry_devv <Victus@DESKTOP-7E1DBLR>
2026-05-11 17:31:53 -0600

    Clases

---
+++ Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MangaNya
+{
+    internal class Cliente
+    {
+
+        string Nit { get; set; } = string.Empty;
+        string nombre { get; set; } = string.Empty;
+        string apellido { get; set; } = string.Empty;
+        string direccion { get; set; } = string.Empty;
+        string telefono { get; set; } = string.Empty;
+
+    }
+}
---
+++ Factura.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MangaNya
+{
+    internal class Factura
+    {
+        string numeroFactura { get; set; } = string.Empty;
+        string fecha { get; set; } = string.Empty;
+        string cliente { get; set; } = string.Empty;
+        string nit { get; set; } = string.Empty;
+        string producto { get; set; } = string.Empty;
+        string cantidad { get; set; } = string.Empty;
+        string precioUnitario { get; set; } = string.Empty;
+        string total { get; set; } = string.Empty;
+    }
+}
